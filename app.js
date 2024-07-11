f33fee2cbf10c5260ce04afd41e4d064ca93421b
mrannoyedhulk <mrannoyedhulk@gmail.com>
2024-07-11 23:55:27 +0800

    Update project to use Globe.gl for visualizing cyber attacks to and from Singapore

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-const COUNTRY = 'Singapore';
+const COUNTRY = 'United States';
 const OPACITY = 0.22;
-const SINGAPORE_COORDINATES = { lat: 1.3521, lng: 103.8198 };
 
 const myGlobe = Globe()
-  (document.getElementById('globeViz'))
-  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-  .pointOfView({ lat: SINGAPORE_COORDINATES.lat, lng: SINGAPORE_COORDINATES.lng, altitude: 2 }) // aim at Singapore
-  .arcDashLength(0.25)
-  .arcDashGap(1)
-  .arcDashInitialGap(() => Math.random())
-  .arcDashAnimateTime(4000)
-  .arcColor(d => [`rgba(0, 255, 0, ${OPACITY})`, `rgba(255, 0, 0, ${OPACITY})`])
-  .arcsTransitionDuration(0)
-  .pointColor(() => 'orange')
-  .pointAltitude(0)
-  .pointRadius(0.02)
-  .pointsMerge(true);
+(document.getElementById('globeViz'))
 
-// Sample data representing attacks to and from Singapore
-const attacks = [
-  { from: { lat: 34.0522, lng: -118.2437 }, to: SINGAPORE_COORDINATES }, // Attack from LA to Singapore
-  { from: { lat: 40.7128, lng: -74.0060 }, to: SINGAPORE_COORDINATES },  // Attack from NY to Singapore
-  { from: SINGAPORE_COORDINATES, to: { lat: 55.7558, lng: 37.6176 } },  // Attack from Singapore to Moscow
-  { from: SINGAPORE_COORDINATES, to: { lat: 35.6895, lng: 139.6917 } }  // Attack from Singapore to Tokyo
-];
+.globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+.pointOfView({ lat: 39.6, lng: -98.5, altitude: 2 }) // aim at continental US centroid
 
-// Load data and update globe visualization
-const pointsData = attacks.flatMap(attack => [attack.from, attack.to]);
+.arcLabel(d => `${d.airline}: ${d.srcIata} &#8594; ${d.dstIata}`)
+.arcStartLat(d => +d.srcAirport.lat)
+.arcStartLng(d => +d.srcAirport.lng)
+.arcEndLat(d => +d.dstAirport.lat)
+.arcEndLng(d => +d.dstAirport.lng)
+.arcDashLength(0.25)
+.arcDashGap(1)
+.arcDashInitialGap(() => Math.random())
+.arcDashAnimateTime(4000)
+.arcColor(d => [`rgba(0, 255, 0, ${OPACITY})`, `rgba(255, 0, 0, ${OPACITY})`])
+.arcsTransitionDuration(0)
 
-const arcsData = attacks.map(attack => ({
-  startLat: attack.from.lat,
-  startLng: attack.from.lng,
-  endLat: attack.to.lat,
-  endLng: attack.to.lng,
-  color: (attack.from === SINGAPORE_COORDINATES) ? ['rgba(255, 0, 0, 0.5)', 'rgba(0, 255, 0, 0.5)'] : ['rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)']
-}));
+.pointColor(() => 'orange')
+.pointAltitude(0)
+.pointRadius(0.02)
+.pointsMerge(true);
 
-myGlobe.pointsData(pointsData).arcsData(arcsData);
+// load data
 
-// Optional: Animation for missile trajectories
-function animateMissile(attack) {
-  const { from, to } = attack;
-  const missileData = [{
-    startLat: from.lat,
-    startLng: from.lng,
-    endLat: to.lat,
-    endLng: to.lng,
-    color: ['yellow']
-  }];
+const airportParse = ([airportId, name, city, country, iata, icao, lat, lng, alt, timezone, dst, tz, type, source]) => ({ airportId, name, city, country, iata, icao, lat, lng, alt, timezone, dst, 
+tz, type, source });
+const routeParse = ([airline, airlineId, srcIata, srcAirportId, dstIata, dstAirportId, codeshare, stops, equipment]) => ({ airline, airlineId, srcIata, srcAirportId, dstIata, dstAirportId, 
+codeshare, stops, equipment});
 
-  myGlobe.arcsData(missileData);
-}
+Promise.all([
+fetch('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat').then(res => res.text())
+.then(d => d3.csvParseRows(d, airportParse)),
+fetch('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat').then(res => res.text())
+.then(d => d3.csvParseRows(d, routeParse))
+]).then(([airports, routes]) => {
 
-attacks.forEach(attack => animateMissile(attack));
+const byIata = indexBy(airports, 'iata', false);
+
+const filteredRoutes = routes
+.filter(d => byIata.hasOwnProperty(d.srcIata) && byIata.hasOwnProperty(d.dstIata)) // exclude unknown airports
+.filter(d => d.stops === '0') // non-stop flights only
+.map(d => Object.assign(d, {
+srcAirport: byIata[d.srcIata],
+dstAirport: byIata[d.dstIata]
+}))
+.filter(d => d.srcAirport.country === COUNTRY && d.dstAirport.country !== COUNTRY); // international routes from country
+
+myGlobe
+.pointsData(airports)
+.arcsData(filteredRoutes);
+});
 
