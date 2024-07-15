cae35093f3915b516ff8ba7bb636d1475c9e4516
mrannoyedhulk <mrannoyedhulk@gmail.com>
2024-07-15 14:27:52 +0800

    Update to include minor changes

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ function updateClock() {
     const now = new Date();
     const timeString = now.toLocaleTimeString();
     const dateString = now.toLocaleDateString();
-    document.getElementById('clock').innerHTML = `${dateString}<br>${timeString}`;
+    document.getElementById('clock').innerHTML = `Local Time<br>${timeString}`;
 }
 
 setInterval(updateClock, 1000);
